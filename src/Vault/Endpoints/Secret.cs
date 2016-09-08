53623f6ec8b838b235dfeec3e23908f0c93a40ad
Chris Hoffman <choffman@chathamfinancial.com>
2016-09-08 11:11:35 -0700

    Adding integration test piece

---
+++ src/Vault/Endpoints/Secret.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Vault.Endpoints
+{
+    public class Secret<TData>
+    {
+        public string RequestId { get; set; }
+        public string LeaseId { get; set; }
+        public bool Renewable { get; set; }
+        public int LeaseDuration { get; set; }
+        public TData Data { get; set; }
+        public dynamic WrappedInformation { get; set; }
+        public List<string> Warnings { get; set; }
+    }
+}
+
