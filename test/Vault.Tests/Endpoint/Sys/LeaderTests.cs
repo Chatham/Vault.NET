0f1150bd21581b6406aefaf5041286ec065e766e
Chris Hoffman <choffman@chathamfinancial.com>
2016-09-22 22:23:45 -0400

    Adding leader tests

---
+++ test/Vault.Tests/Endpoint/Sys/LeaderTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Vault.Tests.Endpoint.Sys
+{
+    public class LeaderTests
+    {
+        [Fact]
+        public async Task Leader_Call_ReturnsLeader()
+        {
+            using (var server = new VaultTestServer())
+            {
+                var client = server.TestClient();
+
+                var leader = await client.Sys.Leader();
+                
+                Assert.NotNull(leader);
+                Assert.False(leader.IsSelf);
+                Assert.False(leader.HaEnables);
+                Assert.NotNull(leader.LeaderAddress);
+            }
+        }
+    }
+}
