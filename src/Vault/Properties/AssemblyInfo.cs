23018ab3f88be8111a80e464ff9628b6b83c36e9
Chris Hoffman <choffman@chathamfinancial.com>
2016-11-30 11:36:54 -0500

    Updating version and assembly info

--- src/Vault/Properties/AssemblyInfo.cs
+++ src/Vault/Properties/AssemblyInfo.cs
@@ -5,10 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyTitle("Vault.NET")]
+[assembly: AssemblyDescription(".NET API for Vault (https://www.vaultproject.io/)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vault")]
+[assembly: AssemblyCompany("Chatham Financial Corp.")]
+[assembly: AssemblyProduct("Vault.NET")]
+[assembly: AssemblyCopyright("Copyright 2016 Chatham Financial Corp.")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
