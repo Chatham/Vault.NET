aba9a6d1220ec2fe30511bdd9c619745e3329e20
Chris Hoffman <choffman@chathamfinancial.com>
2016-12-23 21:23:52 -0500

    minor edits

--- src/Vault/Endpoints/IEndpoint.cs
+++ src/Vault/Endpoints/IEndpoint.cs
@@ -13,10 +13,12 @@ namespace Vault.Endpoints
         Task Write<TParameters>(string path, TParameters data, CancellationToken ct = default(CancellationToken));
         Task<VaultResponse<TData>> Write<TData>(string path, CancellationToken ct = default(CancellationToken));
         Task<VaultResponse<TData>> Write<TParameters, TData>(string path, TParameters data, CancellationToken ct = default(CancellationToken));
+        Task<VaultResponse<TData>> Unwrap<TData>(string wrappingToken, CancellationToken ct = default(CancellationToken));
+
+        // Wrapped requests
         Task<WrappedVaultResponse> Read(string path, TimeSpan wrapTtl, CancellationToken ct = default(CancellationToken));
         Task<WrappedVaultResponse> List(string path, TimeSpan wrapTtl, CancellationToken ct = default(CancellationToken));
         Task<WrappedVaultResponse> Write(string path, TimeSpan wrapTtl, CancellationToken ct = default(CancellationToken));
         Task<WrappedVaultResponse> Write<TParameters>(string path, TParameters data, TimeSpan wrapTtl, CancellationToken ct = default(CancellationToken));
-        Task<VaultResponse<TData>> Unwrap<TData>(string wrappingToken, CancellationToken ct = default(CancellationToken));
     }
 }
