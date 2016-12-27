2a57fb14636a0e82d9f32c4f43325d3477732cf3
Chris Hoffman <choffman@chathamfinancial.com>
2016-12-27 09:44:12 -0500

    Adding DI Extension

---
+++ src/Vault/VaultServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Vault
+{
+    public static class LoggingServiceCollectionExtensions
+    {
+        public static IServiceCollection AddVault(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.TryAdd(ServiceDescriptor.Singleton<IVaultHttpClient, VaultHttpClient>());
+            services.TryAdd(ServiceDescriptor.Singleton<IVaultClient, VaultClient>());
+
+            return services;
+        }
+    }
+}
